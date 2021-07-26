164488626cd710e9e4721d5e765b1bb15bcca00c
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-26 20:18:38 +0530

    post a new article using postman

--- app.js
+++ app.js
@@ -36,6 +36,25 @@ app.get("/articles", function(req, res) {
     }
   });
 });
+
+app.post("/articles",function(req,res){
+    console.log(req.body.title);
+    console.log(req.body.content);
+
+    const newArticle = new Article({
+        title:req.body.title,
+        content:req.body.content
+    });
+
+    newArticle.save(function(err){
+        if(!err){
+            res.send("Succesfully added new article");
+        } else {
+            res.send(err);
+        }
+    });
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
