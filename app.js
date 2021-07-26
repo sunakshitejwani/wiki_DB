d69a937e0391c4dd5bfdd22802e95fbb57a122b6
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-26 21:13:07 +0530

    delete a specific article

--- app.js
+++ app.js
@@ -108,7 +108,16 @@ app
         }
       }
     );
-  });
+  })
+  .delete(function(req,res){
+      Article.deleteOne({title:req.params.articleTitle},function(err){
+          if(!err){
+              res.send("Successfully delete the article");
+          } else {
+              res.send(err);
+          }
+      })
+  })
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
