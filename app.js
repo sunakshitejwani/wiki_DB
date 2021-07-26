176019942ea66ee3e56de882c577d6acf605759a
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-26 21:10:08 +0530

    patch a specific article

--- app.js
+++ app.js
@@ -83,13 +83,26 @@ app
     });
   })
   .put(function(req, res) {
-    Article.update(
+    Article.updateOne(
       { title: req.params.articleTitle },
       { title: req.body.title, content: req.body.content },
       { overwrite: true },
       function(err) {
         if (!err) {
-          res.send("Successfully updated articles");
+          res.send("Successfully updated article");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  })
+  .patch(function(req, res) {
+    Article.updateOne(
+      { title: req.params.articleTitle },
+      { $set: req.body },
+      function(err) {
+        if (!err) {
+          res.send("Successfully updated article");
         } else {
           res.send(err);
         }
