90e2e6501fe7135b913d4869d26de491e2825046
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-26 21:04:08 +0530

    put a specific article

--- app.js
+++ app.js
@@ -82,8 +82,20 @@ app
       }
     });
   })
-  .post(function(req, res) {})
-  .delete(function(req, res) {});
+  .put(function(req, res) {
+    Article.update(
+      { title: req.params.articleTitle },
+      { title: req.body.title, content: req.body.content },
+      { overwrite: true },
+      function(err) {
+        if (!err) {
+          res.send("Successfully updated articles");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
