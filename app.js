934f4961d5b42a1f653d099d0b4d702fb4ba4e1c
sunakshitejwani <sunakshitejwani@gmail.com>
2021-07-26 20:34:42 +0530

    implement chain routes using app.route method

--- app.js
+++ app.js
@@ -26,44 +26,44 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res) {
-  Article.find(function(err, foundArticles) {
-    if (!err) {
-      console.log(foundArticles);
-      res.send(foundArticles);
-    } else {
-      console.log(err);
-    }
-  });
-});
+app
+  .route("/articles")
+  .get(function(req, res) {
+    Article.find(function(err, foundArticles) {
+      if (!err) {
+        console.log(foundArticles);
+        res.send(foundArticles);
+      } else {
+        console.log(err);
+      }
+    });
+  })
+  .post(function(req, res) {
+    console.log(req.body.title);
+    console.log(req.body.content);
 
-app.post("/articles", function(req, res) {
-  console.log(req.body.title);
-  console.log(req.body.content);
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
 
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send("Succesfully added new article");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function(req, res) {
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send("Deleted all articles successfully");
+      } else {
+        res.send(err);
+      }
+    });
   });
-
-  newArticle.save(function(err) {
-    if (!err) {
-      res.send("Succesfully added new article");
-    } else {
-      res.send(err);
-    }
-  });
-});
-
-app.delete("/articles", function(req, res) {
-  Article.deleteMany(function(err) {
-    if (!err) {
-      res.send("Deleted all articles successfully");
-    } else {
-      res.send(err);
-    }
-  });
-});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
